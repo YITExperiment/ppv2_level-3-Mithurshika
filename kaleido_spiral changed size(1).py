0376bb7d20f6a49dc15df30db1dec29dae103405
Mithushika <134926871+Mithurshika@users.noreply.github.com>
2023-06-23 06:19:31 +0530

    Add files via upload

--- kaleido_spiral changed size(1).py
+++ kaleido_spiral changed size(1).py
@@ -10,7 +10,7 @@ def draw_circle(size,angle,shift):
     turtle.forward(shift)
     draw_circle(size+5, angle+1,shift+1)
 
-turtle.bgcolor("blue")
+turtle.bgcolor("black")
 turtle.speed("fast")
-turtle.pensize(4)
+turtle.pensize(40)
 draw_circle(30,0,1)
